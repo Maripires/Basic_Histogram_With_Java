8e6f5c6b1c5e0c9f047fd0554a8b8470c3a4aa5b
Marilize Pires <marilizepires@yahoo.com.br>
2018-01-24 19:05:48 -0800

    Basic histogram

---
+++ DrawHistogram.java
@@ -0,0 +1,72 @@
+
+//Harini Subbiah
+//Marilize Pires
+// Assignment 4 BASIC: Histogram
+//F17 CS F001A OBJ-ORIENTED JAVA  GROUP 12 FALL 2017
+
+import becker.robots.*;
+
+class HistogramRobot extends Robot {
+	// CONSTRUCTOR FOR HistogramRobot
+	HistogramRobot(City c, int st, int ave, Direction dir, int num) {
+		super(c, st, ave, dir, num);
+	}
+
+	public void drawRow(int width) {
+
+		for (int i = 0; i < width; ++i) {
+			putThing();
+			move();
+		}
+		turnLeft(2);
+		for (int j = 0; j < width; ++j) {
+			move();
+		}
+		turnLeft(1);
+		move();
+		turnLeft(1);
+	}
+
+	public void turnLeft(int turns) {
+		int i = 1;
+		while (i <= turns) {
+			turnLeft();
+			i++;
+		}
+	}
+}
+
+public class DrawHistogram extends Object {
+	public static void main(String[] args) {
+		City Edmonds = new City(12, 12);
+		HistogramRobot drawingBot = new HistogramRobot(Edmonds, 0, 1, Direction.EAST, 1000); // drawingBot
+																								// object
+																								// created
+
+		HistogramPrinter histPrinter = new HistogramPrinter(); // histPrinter
+																// object
+																// created to
+																// use the
+																// printHistogram()
+																// method
+
+		// This is the histogramData array with a length of 7 elements
+		int[] histogramData = new int[7];
+
+		histogramData[0] = 3; // The first element holds 3
+		histogramData[1] = 5; // The second element holds 5
+		histogramData[2] = 1; // The third element holds 1
+		histogramData[3] = 0; // The fourth element holds 0
+		histogramData[4] = 4; // The fifth element holds 4
+		histogramData[5] = 2; // The sixth element holds 2
+		histogramData[6] = 1; // The seventh element holds 1
+
+		for (int i = 0; i < histogramData.length; ++i) {
+			drawingBot.drawRow(histogramData[i]);
+		}
+
+		System.out.println(" ============  YOUR OUTPUT STARTS BELOW: ==============");
+
+		histPrinter.printHistogram(histogramData);
+	}
+}
---
+++ HistogramRobot.java
@@ -0,0 +1,22 @@
+//Harini Subbiah
+//Marilize Pires
+// Assignment 4 BASIC: Histogram
+//F17 CS F001A OBJ-ORIENTED JAVA  GROUP 12 FALL 2017
+
+class HistogramPrinter extends Object {
+
+	public void printHistogram(int[] nums) {
+		for (int j = 0; j < nums.length; ++j) {
+			this.printRow(nums[j]);
+		}
+	}
+
+	public void printRow(int width) {
+
+		for (int i = 0; i < width; ++i) {
+			System.out.print("*");
+		}
+		System.out.println(" "); // Drop down to the next line
+
+	}
+}
